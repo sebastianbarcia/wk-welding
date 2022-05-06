68876ca55971b90216728dff18e0faea720debaf
sebastianbarcia <sebastianbarcia.97@gmail.com>
2022-05-06 14:54:41 +0200

    creacion de app utilizando el CLI de WK-welding

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>       
+        <h1>
+         WK Welding
+        </h1>
+        <p>
+          Empresa dedicada a la soldadoras
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
