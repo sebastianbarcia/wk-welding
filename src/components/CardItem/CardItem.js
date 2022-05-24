b908604ae7dc6312793285ba78efded34a0fa1df
sebastianbarcia <sebastianbarcia.97@gmail.com>
2022-05-24 10:29:56 +0200

    add promise in cardContainer / map in cardItemsListContainer

--- src/components/CardItem/CardItem.js
+++ src/components/CardItem/CardItem.js
@@ -2,27 +2,27 @@ import { Button } from "@mui/material";
 import { useState } from "react";
 
 const ItemCard = ({stock , title}) =>{
-    const [count, setCount] = useState(1)
+    const [prev, setCount] = useState(1)
    
     const addToCart = () => {
-        if(count < stock){
-        setCount(count + 1)
+        if(prev < stock){
+        setCount(prev => prev + 1)
         }
     }
     const removeCart = () => {
-        if(count !== 1){
-        setCount(count - 1)
+        if(prev !== 1){
+        setCount(prev => prev - 1)
         }
     }
     const onAddToCart = () => {
-        console.log(`Haz agregado ${count} unidades del producto ${title} quedan en stock ${stock - count}`)
+        console.log(`Haz agregado ${prev} unidades del producto ${title} quedan en stock ${stock - prev}`)
     }
 
     return(
         <>
             <div className="btn-quantity">
                 <Button id="bt--quantity" size="small" onClick={removeCart}> - </Button>
-                <span>{count}</span>
+                <span>{prev}</span>
                 <Button id="bt--quantity" size="small" onClick={addToCart}> + </Button>
             </div>
             <div>
